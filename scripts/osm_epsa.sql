dd866dff10d1287bc00c4b2875530e55e946f10a
Willie Maddox <willie.maddox@gmail.com>
2016-07-11 05:19:38 -0500

    more work on osm sql scripts

--- scripts/osm_epsa.sql
+++ scripts/osm_epsa.sql
@@ -1,11 +1,31 @@
+
+DROP TABLE IF EXISTS buildings0;
+DROP TABLE IF EXISTS goodh;
 DROP TABLE IF EXISTS buildings;
 
-CREATE TABLE buildings AS SELECT 
+CREATE TABLE buildings0 AS SELECT
 	osm_id,
-	building AS type,
 	name,
-	tags->'height' AS height,
-	way
+	way,
+	building AS type,
+	tags->'height' AS height
 	FROM planet_osm_polygon WHERE building IS NOT NULL;
 
+CREATE TABLE goodh AS SELECT
+    osm_id,
+    CAST (height AS NUMERIC)
+    FROM buildings0 where height ~ E'^([0-9.]+)$';
+
+CREATE TABLE buildings AS SELECT
+	p.osm_id,
+	p.name,
+	p.way,
+	p.type AS type,
+	h.height AS height
+	FROM buildings0 p LEFT OUTER JOIN goodh h ON p.osm_id = h.osm_id;
+
 ALTER TABLE buildings ADD PRIMARY KEY (osm_id);
+
+DROP TABLE IF EXISTS goodh;
+DROP TABLE IF EXISTS buildings0;
+
